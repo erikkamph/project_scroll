b067c6832844ae1b35c5a2dd5878d23a912e27ed
Erik Kamph <erik.kamph38@gmail.com>
2022-08-19 12:02:57 +0200

    Scrolling UP or DOWN in a text is now implemented, as well as string from class which will a number of viewable lines depending on width of the terminal and number of viewable lines specified.

---
+++ src/scroll_zerobeef/modules/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from modules.term import terminal_size, config, set_preferred, test_column_size
+from modules.scroll.ScrollManager import ScrollManager
